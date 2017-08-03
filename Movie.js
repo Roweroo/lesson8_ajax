bade1e0177f9a6d07638507672c97888ae50f5ea
Linda Rowe <lindaroo508@gmail.com>
2017-08-03 22:40:52 +0000

    lesson 8 second commit

--- Movie.js
+++ Movie.js
@@ -32,7 +32,7 @@ $(document).ready(function(){
 
             $.ajax({
                 //Insert YOUR API Key after the '=' inside the single quotes.
-                url:'https://api.themoviedb.org/3/search/movie?api_key=INSERT-HERE',
+                url:'https://api.themoviedb.org/3/search/movie?api_key=ac51b221e84164a14ea2fb6ea2aa3057',
                 data: query
             })
             
@@ -57,6 +57,7 @@ $(document).ready(function(){
                  
                  *********************************Insert Code Below********************************************/                
                 
+                 $("#overview").html(json.results[0].overview);
                 
                 
 
@@ -71,7 +72,8 @@ $(document).ready(function(){
                  3.  Use the example above where the Movie Title was retrieved to complete this step!
                  
                  *********************************Insert Code Below********************************************/               
-
+                
+                 $("#release_date").html(json.results[0].release_date);
 
 
 
@@ -86,7 +88,8 @@ $(document).ready(function(){
                  3.  Use the example above where the Movie Title was retrieved to complete this step!
                  
                  *********************************Insert Code Below********************************************/   
-
+                
+                 $("#popularity").html(json.results[0].popularity);
   
 
                 
@@ -101,7 +104,8 @@ $(document).ready(function(){
                  3.  Use the example above where the Movie Title was retrieved to complete this step!
                  
                  *********************************Insert Code Below********************************************/                 
-
+                 
+                 $("#original_language").html(json.results[0].original_language);
 
 
                 //The following code retrieves the movie poster path from the JSON object retrieved from the TMDB 
